837fc82f7d54dd46351383a4851ad8de3574325b
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-13 12:30:22 +0530

    MIN and Max of array

---
+++ DSA/Array/MaxAndMinArray/program.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int min(int arr[], int n)
+{
+    int min = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+int max(int arr[], int n)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main()
+{
+
+    return 0;
+}
