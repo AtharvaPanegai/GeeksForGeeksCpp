f1d02b3a68bf5cdb58d782d4749b7195f6240385
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-15 21:10:48 +0530

    reverse array

--- DSA/Array/reverseArray/program.cpp
+++ DSA/Array/reverseArray/program.cpp
@@ -1,21 +1,16 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int *reverseArray(int arr[],int n){
-    
-}
-
 int main()
 {
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int n = 10;
-    reverse(arr,arr+n);
+    reverse(arr, arr + n);
 
     for (int i = 0; i < n; i++)
     {
-        cout<<arr[i];
+        cout << arr[i];
     }
-    
 
     return 0;
 }
