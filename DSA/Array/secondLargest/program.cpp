78c35dfe250e86595939faa314833a06dfc9e0a6
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-15 20:48:19 +0530

    Min and Max array

--- DSA/Array/secondLargest/program.cpp
+++ DSA/Array/secondLargest/program.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int secondLargest(int arr[], int n)
 {
+    int max = INT_MIN;
+    int secMax = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]>max){
+            max = 
+        }
+    }
+    
 }
 
 int main()
