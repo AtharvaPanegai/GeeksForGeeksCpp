ec56e6fac38ec4e5430c0e10f80f87031cc4598f
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-15 21:08:45 +0530

    Reverse Array

---
+++ DSA/Array/reverseArray/program.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int *reverseArray(int arr[],int n){
+    
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = 10;
+    reverse(arr,arr+n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i];
+    }
+    
+
+    return 0;
+}
