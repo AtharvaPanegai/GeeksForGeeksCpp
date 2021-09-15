c7e8505d12d5793c61ee046e9996ab62df1a475e
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-15 20:53:40 +0530

    sum of Array

---
+++ DSA/Array/sumOfArrayElements/program.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int sumOfArray(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = 10;
+    cout << sumOfArray(arr, n);
+
+    return 0;
+}
