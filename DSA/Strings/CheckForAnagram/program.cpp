af2e71c50b35a4407def307722f3ec8f16b418d9
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-24 17:12:37 +0530

    Anagram Palindrome

---
+++ DSA/Strings/CheckForAnagram/program.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define noOFchars 256
+
+bool isAnagramPalindrome(string s)
+{
+    // create an array to store element's count
+    int count[noOFchars] = {0};
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        count[s[i]]++;
+    }
+
+    int odd = 0;
+
+    for (int i = 0; i < noOFchars; i++)
+    {
+        if (count[i] % 2 == 1)
+        {
+            odd++;
+        }
+    }
+
+    if (odd > 1)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    cout << isAnagramPalindrome("geeksogeeks");
+
+    return 0;
+}
