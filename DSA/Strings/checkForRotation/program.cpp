f362815fcb58a1da60b7200f0afded0a7701ab2f
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-25 13:22:38 +0530

    Check for rotation

---
+++ DSA/Strings/checkForRotation/program.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+bool isRotation(string s1, string s2)
+{
+    if (s1.length() != s2.length())
+    {
+        return false;
+    }
+
+    string temp = s1 + s2;
+    return (temp.find(s2) != string::npos);
+}
+
+int main()
+{
+    string s1 = "geeksforgeeks";
+    string s2 = "forgeeksgeeks";
+    cout << isRotation(s1, s2);
+
+    return 0;
+}
