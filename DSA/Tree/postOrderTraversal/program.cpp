08070bb4435dcd3937a20a7a635868757266c567
AtharvaPanegai <panegai.atharv@gmail.com>
2021-11-05 12:24:59 +0530

    post Order Traversal

---
+++ DSA/Tree/postOrderTraversal/program.cpp
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left, *right;
+
+    // constructor
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void postOrderTraversal(Node *root)
+{
+
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->right->left = new Node(5);
+    root->right->left->left = new Node(7);
+    root->right->right = new Node(6);
+
+    postOrderTraversal(root);
+    return 0;
+}
