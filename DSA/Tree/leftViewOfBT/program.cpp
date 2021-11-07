59f3eb7afc57d09d18f9072dd7958c9384970264
AtharvaPanegai <panegai.atharv@gmail.com>
2021-11-07 23:35:03 +0530

    Left View of BT

---
+++ DSA/Tree/leftViewOfBT/program.cpp
@@ -0,0 +1,67 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left, *right;
+
+    // constructor
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printLeftView(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        int n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            Node *node = q.front();
+            q.pop();
+
+            if (i == 0)
+            {
+                cout << node->data << " ";
+            }
+
+            if (node->left != NULL)
+            {
+                q.push(node->left);
+            }
+            if (node->right != NULL)
+            {
+                q.push(node->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right = new Node(3);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    printLeftView(root);
+
+    return 0;
+}
