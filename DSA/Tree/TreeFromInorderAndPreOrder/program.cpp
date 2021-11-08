7452b3f4a735dbf68ebc7c2cf96f7414e79c001d
AtharvaPanegai <panegai.atharv@gmail.com>
2021-11-08 18:56:37 +0530

    InOrder and preOrder

--- DSA/Tree/TreeFromInorderAndPreOrder/program.cpp
+++ DSA/Tree/TreeFromInorderAndPreOrder/program.cpp
@@ -15,12 +15,57 @@ struct Node
     }
 };
 
+int search(int inOrder[], int start, int end, int curr)
+{
+    for (int i = start; i <= end; i++)
+    {
+        if (inOrder[i] == curr)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 Node *buildTree(int inOrder[], int preOrder[], int start, int end)
 {
+    static int idx = 0;
+    if (start > end)
+    {
+        return NULL;
+    }
+    int curr = preOrder[idx];
+    idx++;
+    Node *root = new Node(curr);
+    int pos = search(inOrder, start, end, curr);
+    if (start == end)
+    {
+        return root;
+    }
+    root->left = buildTree(inOrder, preOrder, start, pos - 1);
+    root->right = buildTree(inOrder, preOrder, pos + 1, end);
+
+    return root;
+}
+
+void inOrderPrint(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrderPrint(root->left);
+    cout << root->data << " ";
+    inOrderPrint(root->right);
 }
 
 int main()
 {
+    int preOrder[] = {1, 2, 4, 3, 5};
+    int inOrder[] = {4, 2, 1, 5, 3};
+
+    Node *root = buildTree(inOrder,preOrder,0,4);
+    inOrderPrint(root);
 
     return 0;
 }
